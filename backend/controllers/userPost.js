08d803802e21138d40c448832fdbcb6521bfbea3
Bibek Sah <bibekkumarsah.456@gmail.com>
2025-02-07 21:21:58 +0545

    like and dislike api

--- backend/controllers/userPost.js
+++ backend/controllers/userPost.js
@@ -2,8 +2,8 @@ import { postModels } from "../models/postSchema.js";
 
 export const user_Post = async (req, res) => {
   try {
-    const {description, id } = req.body;
-    if ( !description || !id) {
+    const { description, id } = req.body;
+    if (!description || !id) {
       return res.status(401).json({
         message: "All field are required",
         success: false,
@@ -11,8 +11,8 @@ export const user_Post = async (req, res) => {
     }
     await postModels.create({
       description,
-      userid:id
-    })
+      userid: id,
+    });
 
     return res.status(201).json({
       message: "successfully post created",
@@ -23,15 +23,42 @@ export const user_Post = async (req, res) => {
   }
 };
 
-export const delete_post=async(req,res)=>{
-  try{
-   await postModels.findByIdAndDelete({_id:req.params.id})
-   res.status(201).json({
-    message:"post have been deleted successfully",
-    success:true
-   })
-  }catch(error){
-    console.log(error)
+export const delete_post = async (req, res) => {
+  try {
+    await postModels.findByIdAndDelete({ _id: req.params.id });
+    res.status(201).json({
+      message: "post have been deleted successfully",
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
   }
-}
+};
 
+export const likeOrDislike = async (req, res) => {
+  try {
+    const loginUseerId = req.body.id;
+    const postId = req.params.id;
+    const post = await postModels.findById(postId);
+    if(!post){
+      return res.status(401).json({
+        message:"post not exits"
+      })
+    }
+    if (post.likes.includes(loginUseerId)) {
+      //dislikes
+      await postModels.findByIdAndUpdate(postId,{ $pull: { likes: loginUseerId } });
+      return res.status(201).json({
+        message: "You disliked userpost post",
+      });
+    } else {
+      //likes
+      await postModels.findByIdAndUpdate(postId,{ $push: { likes: loginUseerId } });
+       return res.status(201).json({
+         message: "You liked userpost post",
+       });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
--- backend/routes/userPost.js
+++ backend/routes/userPost.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { delete_post, user_Post } from "../controllers/userPost.js";
+import {
+  delete_post,
+  likeOrDislike,
+  user_Post,
+} from "../controllers/userPost.js";
 import { isAuthincated } from "../middleware/Auth.js";
 
 export const userPostRoutes = express.Router();
 
 userPostRoutes.post("/post/socialmedia", isAuthincated, user_Post);
-userPostRoutes.delete("/delete/:id",  delete_post);
+userPostRoutes.delete("/delete/:id", isAuthincated, delete_post);
+userPostRoutes.post("/likesOrDislikes/:id", isAuthincated, likeOrDislike);
